bfe020c5faea14050febec078700bc105355b601
ofan sofian <ofansofian17@gmail.com>
2022-06-11 20:28:35 +0700

    feat: Done tugas challenge-06- Ofan Sofian

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+let fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,27 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let data = [];
+
+  fs.readFile(file1, (err, data1) => {
+    if (err) return fnCallback(err);
+    data.push(JSON.parse(data1)?.message?.split(" ")[1]);
+
+    fs.readFile(file2, (err, data2) => {
+      if (err) return fnCallback(err);
+      data.push(JSON.parse(data2)[0]?.message?.split(" ")[1]);
+
+      fs.readFile(file3, (err, data3) => {
+        if (err) return fnCallback(err);
+        data.push(JSON.parse(data3)[0]?.data?.message?.split(" ")[1]);
+
+        fnCallback(null, data);
+      });
+    });
+  })
+};
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
